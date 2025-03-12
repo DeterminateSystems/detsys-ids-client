3c5e623ba0bf590de36c3e3bf70bb041bbcdd7a2
Graham Christensen <graham@grahamc.com>
2025-03-11 20:11:32 -0400

    Move the generic snapshotter into Default so it can be reused

--- src/system_snapshot/mod.rs
+++ src/system_snapshot/mod.rs
@@ -1,3 +1,5 @@
+use std::io::IsTerminal;
+
 use crate::Map;
 
 mod generic;
@@ -43,6 +45,37 @@ pub struct SystemSnapshot {
     /// Additional fields to be flattened into the snapshot data
     #[serde(flatten)]
     pub extra_fields: Option<Map>,
+}
+
+impl Default for SystemSnapshot {
+    fn default() -> Self {
+        let system = sysinfo::System::new_all();
+
+        let is_ci = is_ci::cached()
+            || std::env::var("DETSYS_IDS_IN_CI").unwrap_or_else(|_| "0".into()) == "1";
+
+        Self {
+            locale: sys_locale::get_locale(),
+            timezone: iana_time_zone::get_timezone().ok(),
+
+            host_name: sysinfo::System::host_name(),
+            operating_system: sysinfo::System::long_os_version(),
+            operating_system_version: sysinfo::System::os_version(),
+
+            target_triple: target_lexicon::HOST.to_string(),
+            stdin_is_terminal: std::io::stdin().is_terminal(),
+            is_ci,
+
+            processor_count: system.physical_core_count().map(
+                |count| count as u64, /* safety: `as` truncates on overflow */
+            ),
+            physical_memory_bytes: system.total_memory(),
+            boot_time: sysinfo::System::boot_time(),
+            process_name: std::env::args().next(),
+
+            extra_fields: None,
+        }
+    }
 }
 
 pub trait SystemSnapshotter: Send + Sync + 'static {
