c60c3c9b6576307573410c407c808615695c776e
Tristan Ross <tristan.ross@determinate.systems>
2026-05-06 17:42:45 -0700

    Drop x86_64-darwin

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
     , ...
     } @ inputs:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
 
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: (forSystem system f));
 
